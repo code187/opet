d593f12bdbe264e7bafe618f5711e2de0682d82d
code187 <code187@users.noreply.github.com>
2014-09-10 15:10:07 +0200

    add rest

--- callajax.php
+++ callajax.php
@@ -4,10 +4,10 @@
 
 //Baza i username i password
 $baza = "gaudeam_knex2013";
-$korisnik = "gaudeam";
-$lozinka = "OF9002089ir.";
+$korisnik = "gaudeam_knex";
+$lozinka = "@00886726@";
 
-$spoj = mysql_connect("178.218.164.4","$korisnik","$lozinka") or die ("<span class=podnaslovi_crveni>GREŠKA 003 - Vaše korisnicko ime ili lozinka za bazu su neispravni!</span>");
+$spoj = mysql_connect("gaudeamus.hr","$korisnik","$lozinka") or die ("<span class=podnaslovi_crveni>GREŠKA 003 - Vaše korisnicko ime ili lozinka za bazu su neispravni!</span>");
 
 $baza = mysql_select_db("$baza", $spoj) or die("<span class=podnaslovi_crveni>GREŠKA 002 - Baza nije pronadena na serveru!</span>");
 
