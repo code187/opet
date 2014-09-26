8b3a8afa974dcaacdbf5b80d2b83fd538e65e34a
code187 <code187@users.noreply.github.com>
2014-09-26 14:06:16 +0200

    bla

--- postavke.php
+++ postavke.php
@@ -1,6 +1,6 @@
 <?php 
 //Baza i username i password
-$baza = "gaudeam_knex2013";
+$baza = "gaudeam_knex";
 $korisnik = "gaudeam_knex";
 $lozinka = "@00886726@";
 $spoj = mysql_connect("localhost","$korisnik","$lozinka") or die ("<span class=podnaslovi_crveni>GREŠKA 003 - Vaše korisnicko ime ili lozinka za bazu su neispravni!</span>");
